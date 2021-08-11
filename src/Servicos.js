0756552ea52225ec0e1edc5d24198b3484098430
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-08-10 23:59:33 -0300

    ajustes finais na tela de servicos

--- src/Servicos.js
+++ src/Servicos.js
@@ -14,27 +14,28 @@ export const Servicos = () => (
               <h2 className="section-title text-white">Nossos Serviços</h2>
             </ScrollAnimation>
             <div className="habilidades_container bd-grid">
-              <div className="habilidades_box">
-                <h3 className="habilidades_subtitle">Desenvolvimento</h3>
+              <div className="habilidades_img mt-16">
+                <img src="https://res.cloudinary.com/robles-identity/image/upload/v1627948326/PicsArt_08-02-08.51.10_atg8fd.png" alt="" />
+              </div>
+              <div className="habilidades_box mt-16">
+                <h3 className="habilidades_subtitle d-none d-md-block">Desenvolvimento</h3>
+                <h3 className="habilidades_subtitle -mt-28 d-lg-none">Desenvolvimento</h3>
                 <span className="habilidades_name">Javascript<img style={{ height: '48px' }} src="https://img.icons8.com/nolan/48/javascript.png" /></span>
                 <span className="habilidades_name">React.js<img style={{ height: '48px' }} src="https://img.icons8.com/nolan/48/react-native.png" /></span>
                 <span className="habilidades_name">Next.js<img style={{ height: '48px' }} src="https://www.creative-tim.com/learning-lab/assets/images/nextjs.png" /></span>
                 <span className="Habilidades_detalhes">
                   <h1 className="Aspas_Detalhe"></h1>
-                  <p className="Habilidades_detalhes" style={{ color: '#1d20ff' }}>
+                  <h1 className="Habilidades_detalhes" style={{ color: '#1d20ff' }}>
                     Olá, meu nome é Adão Oliveira,
                     faço parte do desenvolvimento da Robles Identity que visa gerar identidade ao seu projeto ou empresa.
                     Tomo conta de toda a área de programação, técnica, domínio e hospedagem
-                  </p>
+                  </h1>
                 </span>
-              </div>
-              <div className="habilidades_img">
-                <img src="https://res.cloudinary.com/robles-identity/image/upload/v1627948326/PicsArt_08-02-08.51.10_atg8fd.png" alt="" />
               </div>
             </div>
           </section>
           {/* JULIANA */}
-          <section className="habilidades section -mt-32">
+          <section className="habilidades section">
             <div className="habilidades_container bd-grid">
               <div className="habilidades_img">
                 <img src="https://res.cloudinary.com/robles-identity/image/upload/v1627949836/PicsArt_08-02-09.16.51_veqw2j.png" alt="" />
@@ -46,11 +47,11 @@ export const Servicos = () => (
                 <span className="habilidades_name">Canva<img style={{ height: '48px' }} src="https://img.apksum.com/c0/com.canva.editor/2.76.0/icon.png" /></span>
                 <span className="Habilidades_detalhes">
                   <h1 className="Aspas_Detalhe"></h1>
-                  <p className="Habilidades_detalhes" style={{ color: 'rgb(231, 28, 147)' }}>
+                  <h1 className="Habilidades_detalhes" style={{ color: 'rgb(231, 28, 147)' }}>
                     Olá, meu nome é Juliana Robles,
                     faço parte do desenvolvimento da Robles Identity que visa gerar identidade ao seu projeto ou empresa.
                     Tomo conta de toda área de design, marketing, identidade visual e conteúdo.
-                  </p>
+                  </h1>
                 </span>
               </div>
             </div>
@@ -82,10 +83,13 @@ export const Servicos = () => (
   margin-right: 1rem;
   margin-bottom: 1.5rem;
   padding: .25rem .5rem;
-  font-size: 30px;
   border-radius: .25rem;
-  width: 33rem;
+  width: 400px;
 }
+@media screen and (min-width: 768px){
+  .Habilidades_detalhes{
+    width: 33rem;
+  }
 .Aspas_Detalhe{
   content: "";
   display: block;
